0963279b6a302170c5827f68498e2a6f4220be78
selvamnair <36755442+selvamnair@users.noreply.github.com>
2018-07-24 13:40:49 +0530

    Update and rename selvam1.py to selvam1.c

---
+++ selvam1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(void)
+{
+   int a,i;
+   printf("enter the number");
+   scanf("%d",&a);
+   for(i=1;i<=a;i++)
+   {
+       printf("\nHELLO");
+   }
+   return (0);
+}
